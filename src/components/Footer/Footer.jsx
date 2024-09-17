37fc155b9dcfca05226d6140e529f5a90d5dc42a
kumkum <122430139+kumkum99@users.noreply.github.com>
2024-09-17 11:45:22 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,10 +22,10 @@ const Footer = () => {
           <div className="link-boxes">
             <ul className="box">
               <li className="link_name">Company</li>
-              <li><a href="/">Home</a></li>
-              <li><a href="/contact">Contact us</a></li>
-              <li><a href="/about">About us</a></li>
-              <li><a href="/Services">Services</a></li>
+                <li><a href="/">Home</a></li>
+              <li><Link to="/contact">Contact us</Link></li>
+              <li><Link to="/about">About us</Link></li>
+              <li><Link to="/Services">Services</Link></li>
             </ul>
             <ul className="box">
               <li className="link_name">Services</li>
